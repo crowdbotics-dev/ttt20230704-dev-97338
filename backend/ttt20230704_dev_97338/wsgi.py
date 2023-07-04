dc40606f6a1c7bc4c28aa8ae87a7aeff8926c649
Crowdbotics <engineering@crowdbotics.com>
2023-07-04 13:04:48 +0000

    Initial commit.

---
+++ backend/ttt20230704_dev_97338/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ttt20230704_dev_97338 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ttt20230704_dev_97338.settings')
+
+application = get_wsgi_application()
